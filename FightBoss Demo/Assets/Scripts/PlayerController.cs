fb14c35565a89f9a2a3cc0673b77a95afef70d23
molpha <s2020669@s.tsukuba.ac.jp>
2020-12-16 20:30:43 +0900

    no message

---
+++ FightBoss Demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputController pi;
+    public Animator anim;
+
+    private bool canDash;
+    private bool canAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pi = GetComponent<InputController>( );
+        anim = GetComponent<Animator>( );
+        canDash = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canDash && Input.GetKeyDown( KeyCode.Space )) { 
+            anim.SetTrigger( "dash" );
+        }
+
+        if (Input.GetMouseButtonDown( 0 )) {
+            anim.SetTrigger( "attack" );
+        }
+
+        if (Input.GetKey( KeyCode.LeftShift )) {
+            anim.SetBool( "run", true );
+        } else {
+            anim.SetBool( "run", false );
+        }
+
+        anim.SetFloat( "x_Input", pi.Dvec.x );
+        anim.SetFloat( "y_Input", pi.Dvec.z );
+        anim.SetFloat( "magnitude", pi.Dmag );
+
+    }
+
+    public void OnEnterDash() {
+        canDash = false;
+    }
+
+    public void OnExitDash() {
+        canDash = true;
+    }
+}
---
+++ FightBoss Demo/Assets/Twinblades_Expansion_V2/Script/WeaponMasterUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WeaponMasterUI : MonoBehaviour 
+{
+	public Animator previewAnim;
+	public GameObject prefabButton;
+	public string replaceName;
+	public AnimationClip[] line1_anims;
+	public AnimationClip[] line2_anims;
+	public AnimationClip[] line3_anims;
+
+	void Start ()
+	{
+		prefabButton.SetActive (false);
+
+		//Line1
+		for (int i = 0; i < line1_anims.Length; i++)
+		{
+			ButtonInstantiate(line1_anims[i], 0, i);
+		}
+		//Line2
+		for (int i = 0; i < line2_anims.Length; i++)
+		{
+			ButtonInstantiate(line2_anims[i], 1, i);
+		}
+		//Line2
+		for (int i = 0; i < line3_anims.Length; i++)
+		{
+			ButtonInstantiate(line3_anims[i], 2, i);
+		}
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	private void ButtonInstantiate(AnimationClip anim,int Xindex , int Yindex)
+	{
+		GameObject go = Instantiate (prefabButton, prefabButton.transform.position + new Vector3(160 * Xindex, -30 * Yindex, 0), Quaternion.identity);
+		go.transform.parent = gameObject.transform;
+		go.GetComponentInChildren<Text> ().text = anim.name.Replace (replaceName, "");
+		go.GetComponent<Button> ().onClick.AddListener(delegate() 
+			{ 
+				previewAnim.Rebind();
+				previewAnim.Play(anim.name);
+			});
+		go.SetActive (true);
+	}
+}
