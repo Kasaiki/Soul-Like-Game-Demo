3b0d118c5932ae730b18396782c9ac8fcf163c1d
molpha <s2020669@s.tsukuba.ac.jp>
2021-01-03 12:13:04 +0900

    機能追加 １．当たり判定を追加 ２．scriptによるcolliderコントロールを実現

---
+++ FightBoss Demo/Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public WeaponManager weaponManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponManager = GetComponentInChildren<WeaponManager>( );
+    }
+
+    // Animation Event
+    void StartAttackEvent() {
+        print( "Start Attack");
+        // play sound
+        weaponManager.WeaponEnable = true;
+    }
+
+    void EndAttackEvent() {
+        print( "End Attack" );
+        // stop play sound
+        weaponManager.WeaponEnable = false;
+    }
+}
---
+++ FightBoss Demo/Assets/Scripts/Weapon/WeaponManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public float ATK;
+
+    [SerializeField]
+    private BoxCollider[] weaponColliders;
+
+    private bool weaponEnable = false;
+    private bool lastWeaponEnable = false;
+
+    public bool WeaponEnable {
+        get { return weaponEnable; }
+        set {
+            weaponEnable = value;
+        }
+    }
+
+    private void Awake() {
+        weaponColliders = GetComponents<BoxCollider>( );
+    }
+
+    private void OnTriggerEnter(Collider target) {
+        Debug.Log( target );
+
+        Vector3 location = this.transform.position;
+        Vector3 hitPoint = target.ClosestPointOnBounds( location );
+
+        Debug.Log( hitPoint );
+    }
+
+    private void LateUpdate() {
+        if(weaponEnable != lastWeaponEnable) {
+            foreach (var collider in weaponColliders) {
+                collider.enabled = weaponEnable;
+            }
+        }
+        lastWeaponEnable = weaponEnable;
+    }
+}
