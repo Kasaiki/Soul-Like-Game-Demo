35a20d10b421e9f136503ae3349541cf3a776d66
molpha <s2020669@s.tsukuba.ac.jp>
2021-01-13 00:53:25 +0900

    no message

---
+++ FightBoss Demo/Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager gm;
+
+    public static GameManager getGM {
+        get { return gm; }
+    }
+
+    private void Awake() {
+        gm = this;
+    }
+
+    public void AddHP(int count) {
+        // do something
+    }
+
+    public void GameOver() {
+        // do something
+        SceneManager.LoadScene( "Title" );
+    }
+}
---
+++ FightBoss Demo/Assets/Scripts/System/SceneControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    // スタートボタンを押したら実行する
+    public void StartGame() {
+        SceneManager.LoadScene( "Stage01" );
+    }
+
+    public void ExitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
